0e11294468590148fd96978a009cd6563d65a93a
LyubomirT <terlyubomir@gmail.com>
2023-12-18 21:19:51 +0200

    Add log directory configuration

--- main.py
+++ main.py
@@ -7,17 +7,31 @@ from discord.ext import commands
 from discord.commands import Option
 from discord.ui import Button, View, Select, Modal
 from colorizer import Colorizer
+import configparser
+import asyncio
+from datetime import datetime
 
 load_dotenv()
 
 token = os.getenv("BOT_TOKEN")
 
 bot = commands.Bot(command_prefix="!")
+log_dir = "_logs_"
+
+def load_config():
+    with open("_config_/directories.cfg", "r") as f:
+        config = configparser.ConfigParser()
+        config.read_file(f)
+        global log_dir
+        log_dir = config["directories"]["log_dir"]
 
 @bot.event
 async def on_ready():
-    print(Colorizer("green").colorize("Bot is ready! Using configuration provided in the _config_ folder."))
+    print(Colorizer("cyan").colorize("Bot is ready! Using configuration provided in the _config_ folder."))
     print(Colorizer("yellow").colorize("Bot is running on version 1.0.0"))
+    print(Colorizer("yellow").colorize("Config Preview:"))
+    load_config()
+    print(Colorizer("yellow").colorize("Log directory: " + log_dir))
 
 
 bot.remove_command("help")
