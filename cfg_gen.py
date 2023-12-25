39aceee7afac05a128b791af2e4af13e10b4274d
LyubomirT <terlyubomir@gmail.com>
2023-12-25 11:48:39 +0200

    Update .gitignore and remove unnecessary configuration files. Update cfg generation

--- cfg_gen.py
+++ cfg_gen.py
@@ -1,28 +1,45 @@
 import os
 import shutil
 import configparser
-from colorizer import Colorizer
+from colorizer import Colorizer  # Assuming Colorizer class is defined in colorizer.py
 
 def verify_dir(directory):
     if not os.path.exists(directory):
-        os.makedirs(directory)
+        try:
+            os.makedirs(directory)
+        except OSError:
+            print(Colorizer("red").colorize("Creation of the directory failed. Terminating."))
+            exit(1)
 
 def overwrite_confirmation(directory):
     if os.path.exists(directory):
         response = input(Colorizer("yellow").colorize(
             f"There already seems to be a {directory} directory here. Proceeding will overwrite it COMPLETELY. "
-            f"Do you still want to continue (y/n)? "
+            f"Do you still want to continue (yes/no)? "
         ))
-        return response.lower() == 'y'
-    return True
+        return response.lower() == 'yes' or response.lower() == 'y'
+    return None
+
+def ask(prompt):
+    while True:
+        response = input(prompt).lower()
+        if response in ['yes', 'no', 'y', 'n']:
+            return response == 'yes' or response == 'y'
+        else:
+            print(Colorizer("red").colorize("Invalid input. Please enter 'yes', 'no', 'y', or 'n'."))
 
 def generate_config():
     config_dir = "_config_"
     
-    if not overwrite_confirmation(config_dir):
+    confirmation = overwrite_confirmation(config_dir)
+    if not confirmation:
         print(Colorizer("red").colorize("Operation aborted. Configuration generation canceled."))
         return
-    
+    elif confirmation is True:
+        shutil.rmtree(config_dir)
+        print(Colorizer("cyan").colorize("Removed existing configuration directory."))
+    elif confirmation is None:
+        print(Colorizer("green").colorize("No existing configuration directory found. Creating one."))
     verify_dir(config_dir)
 
     # Directories configuration
@@ -30,22 +47,21 @@ def generate_config():
     verify_dir(log_dir)
 
     # Types configuration
-    dm_enabled = input("Enable direct messages (True/False): ").lower() == 'true'
-    dm_download_images = input("Download images in direct messages (True/False): ").lower() == 'true'
-    dm_download_videos = input("Download videos in direct messages (True/False): ").lower() == 'true'
-    dm_download_audio = input("Download audio in direct messages (True/False): ").lower() == 'true'
+    dm_enabled = ask("Enable direct messages (yes/no): ")
+    dm_download_images = ask("Download images in direct messages (yes/no): ")
+    dm_download_videos = ask("Download videos in direct messages (yes/no): ")
+    dm_download_audio = ask("Download audio in direct messages (yes/no): ")
 
-    server_enabled = input("Enable server messages (True/False): ").lower() == 'true'
-    server_download_images = input("Download images in server messages (True/False): ").lower() == 'true'
-    server_download_videos = input("Download videos in server messages (True/False): ").lower() == 'true'
-    server_download_audio = input("Download audio in server messages (True/False): ").lower() == 'true'
+    server_enabled = ask("Enable server messages (yes/no): ")
+    server_download_images = ask("Download images in server messages (yes/no): ")
+    server_download_videos = ask("Download videos in server messages (yes/no): ")
+    server_download_audio = ask("Download audio in server messages (yes/no): ")
 
     # Misc configuration
-    print_contents = input("Print contents (True/False): ").lower() == 'true'
-    log_to_dms = input("Enable logging to direct messages (True/False): ").lower() == 'true'
+    print_contents = ask("Print contents (yes/no): ")
+    log_to_dms = ask("Enable logging to direct messages (yes/no): ")
     owner_id = int(input("Enter owner ID: "))
 
-    # Write configuration to files
     with open(os.path.join(config_dir, "directories.cfg"), "w") as f:
         f.write(f"[directories]\nlog_dir = {log_dir}\n")
 
@@ -62,8 +78,8 @@ def generate_config():
                 f"download_audio = {server_download_audio}\n")
 
     with open(os.path.join(config_dir, "misc.cfg"), "w") as f:
-        f.write("[Console]\nprintContents = {print_contents}\n\n"
-                "[DiscordLog]\nenabled = {log_to_dms}\nownerid = {owner_id}\n")
+        f.write("[Console]\nprintContents = {}\n\n"
+                "[DiscordLog]\nenabled = {}\nownerid = {}\n".format(print_contents, log_to_dms, owner_id))
 
     print(Colorizer("green").colorize("Configuration files generated successfully."))
 
